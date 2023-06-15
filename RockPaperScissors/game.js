fbf6335008b14a735a4d97d6b10a2b220c324199
Maria Secco <mapradosecco@gmail.com>
2023-06-14 22:42:21 -0300

    created variables for the buttons

--- RockPaperScissors/game.js
+++ RockPaperScissors/game.js
@@ -1,9 +1,15 @@
+const btRock = document.querySelector(".rock")
+const btPaper = document.querySelector(".paper")
+const btScissors = document.querySelector(".scissors")
+
+let value;
+
 function getComputerChoice (){
   let random =  rando(["rock", "paper", "scissors"]); // extension from rando that allows you to receive random values automatically
 
   let number = Object.values(random)
+  value = number[1]
 
-  console.log(number[1])
 }
 getComputerChoice()
 
