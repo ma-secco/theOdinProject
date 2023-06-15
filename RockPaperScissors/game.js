664ab00413296b64f052c56fadef98964d940da8
Maria Secco <mapradosecco@gmail.com>
2023-06-15 13:07:31 -0300

    complemented the functions and descriptions

--- RockPaperScissors/game.js
+++ RockPaperScissors/game.js
@@ -1,10 +1,13 @@
+//naming variables
 const btRock = document.querySelector("#rock")
 const btPaper = document.querySelector("#paper")
 const btScissors = document.querySelector("#scissors")
+const resp = document.querySelector("h3")
 
 let valueComputer;
 let valuePlayer;
 
+// event listener in the buttons
 btRock.addEventListener("click", (e) =>{
   e.preventDefault()
   valuePlayer = "Rock"
@@ -18,6 +21,7 @@ btPaper.addEventListener("click", (e) =>{
     valuePlayer = "Scissors"
   })
 
+// getting the computer selection for the round
 function getComputerChoice (){
   let random =  rando(["Rock", "Paper", "Scissors"]); // extension from rando that allows you to receive random values automatically
 
@@ -25,6 +29,20 @@ function getComputerChoice (){
   valueComputer = number[1]
 }
 
+
+//changing the resp inner text for each situation
+function tie (){
+  resp.innerText = `It's a tie!`
+}
+function playerWin (){
+  resp.innerText = `You Win! ${valuePlayer} beats ${valueComputer}`
+}
+function computerWinWin (){
+  resp.innerText = `You Lose! ${valueComputer} beats ${valuePlayer}`
+}
+
+
+// creating the function for each round
 function playRound(playerSelection, computerSelection) {
   if(computerSelection === "Rock"){
     if (playerSelection == "Rock"){
