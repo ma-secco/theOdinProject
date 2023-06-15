419761d4c07ffd25c9d5624081d9ea7362899117
Maria Secco <mapradosecco@gmail.com>
2023-06-15 12:02:20 -0300

    first attempt for function playRound

--- RockPaperScissors/game.js
+++ RockPaperScissors/game.js
@@ -2,14 +2,31 @@ const btRock = document.querySelector(".rock")
 const btPaper = document.querySelector(".paper")
 const btScissors = document.querySelector(".scissors")
 
-let value;
+let valueComputer;
+let valuePlayer = () => {
+  if (btRock.addEventListener("click")) {
+    valuePlayer = "Rock";
+  }
+  else if (btPaper.addEventListener("click")) {
+    valuePlayer = "Paper";
+  }
+  else {
+    btScissors.addEventListener("click", () =>{
+      valuePlayer = "Scissors"
+    })
+  }
+}
+
+console.log(valuePlayer)
 
 function getComputerChoice (){
   let random =  rando(["rock", "paper", "scissors"]); // extension from rando that allows you to receive random values automatically
 
   let number = Object.values(random)
-  value = number[1]
+  valueComputer = number[1]
 
 }
-getComputerChoice()
 
+function playRound(playerSelection, computerSelection) {
+  
+}
