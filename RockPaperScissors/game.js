6f53a98f13a863d6b5313764efe506cb08826a68
Maria Secco <mapradosecco@gmail.com>
2023-06-26 11:57:04 -0300

    Update game.js

--- RockPaperScissors/game.js
+++ RockPaperScissors/game.js
@@ -1,32 +1,70 @@
-const btRock = document.querySelector(".rock")
-const btPaper = document.querySelector(".paper")
-const btScissors = document.querySelector(".scissors")
+//naming variables
+const btRock = document.querySelector("#rock")
+const btPaper = document.querySelector("#paper")
+const btScissors = document.querySelector("#scissors")
+const resp = document.querySelector("h3")
+const score = document.querySelector("pre")
 
 let valueComputer;
-let valuePlayer = () => {
-  if (btRock.addEventListener("click")) {
-    valuePlayer = "Rock";
-  }
-  else if (btPaper.addEventListener("click")) {
-    valuePlayer = "Paper";
+let valuePlayer;
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+// event listener in the buttons
+btRock.addEventListener("click", (e) =>{
+  e.preventDefault()
+  valuePlayer = "Rock"
+  game()
+})
+btPaper.addEventListener("click", (e) =>{
+  e.preventDefault()
+  valuePlayer = "Paper"
+  game()
+})
+btScissors.addEventListener("click", (e) =>{
+  e.preventDefault()
+  valuePlayer = "Scissors"
+  game()
+})  
+
+
+function game(){
+  if (playerScore == 5 ||computerScore == 5){
+    endGame()
   }
   else {
-    btScissors.addEventListener("click", () =>{
-      valuePlayer = "Scissors"
-    })
-  }
+    getComputerChoice();
+    playRound(valuePlayer, valueComputer);}
 }
 
-console.log(valuePlayer)
-
+// getting the computer selection for the round
 function getComputerChoice (){
-  let random =  rando(["rock", "paper", "scissors"]); // extension from rando that allows you to receive random values automatically
+  let random =  rando(["Rock", "Paper", "Scissors"]); // extension from rando that allows you to receive random values automatically
 
   let number = Object.values(random)
   valueComputer = number[1]
-
 }
 
+
+//changing the resp inner text for each situation
+function tie (){
+  resp.innerText = `It's a tie!`
+}
+function playerWin (){
+  resp.innerText = `You Win! ${valuePlayer} beats ${valueComputer}`
+  playerScore++;
+  score.innerText = `Player score: ${playerScore}\nComputer score: ${computerScore}`  
+}
+function computerWin (){
+  resp.innerText = `You Lose! ${valueComputer} beats ${valuePlayer}`
+  computerScore++;
+  score.innerText = `Player score: ${playerScore}\nComputer score: ${computerScore}`  
+}
+
+
+// function display the results
 function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
     tie ()
